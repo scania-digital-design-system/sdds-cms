d693e0109b64dd71c271e3f796c2c816a28c3f45
Yudhanira Ela <ela.yudhanira@scania.com>
2021-04-13 11:15:59 +0200

    update database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,3 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -7,18 +5,16 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'sdds-cms'),
+        username: env('DATABASE_USERNAME', 'postgress'),
+        password: env('DATABASE_PASSWORD', ''),
         ssl: {
-          rejectUnauthorized: false
+          rejectUnauthorized: false, // For self-signed certificates
         },
       },
-      options: {
-        ssl: true,
-      },
+      options: {},
     },
   },
 });
